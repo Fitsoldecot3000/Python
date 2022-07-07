bc946b5a0c4e3d48e0a19c959c027e69fbe84dd5
Jisashi Said <jisashiramos@gmail.com>
2022-07-07 14:59:24 -0500

    Ejercicio 3 del curso de Open Bootcamp

---
+++ Ejercicio3.py
@@ -0,0 +1,15 @@
+var1 = 3.14
+print("la variable en cuestion es equivalente a")
+print(var1)
+print("y su tipo de variable es")
+print(type(var1))
+var1 = 3
+print("la variable en cuestion es equivalente a")
+print(var1)
+print("y su tipo de variable es")
+print(type(var1))
+var1 = "Buenos dias"
+print("la variable en cuestion es equivalente a")
+print(var1)
+print("y su tipo de variable es")
+print(type(var1))
